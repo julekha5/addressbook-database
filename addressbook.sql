edd432f529c6b1faa8ece5c4eb8d644c67d01660
Julekha5 <mulani.julekha93@gmail.com>
2021-12-27 22:23:29 +0530

    Added : addressbook table created

--- addressbook.sql
+++ addressbook.sql
@@ -1,1 +1,15 @@
 CREATE DATABASE addressbook;
+
+USE addressbook;
+
+CREATE TABLE addressBook (
+		firstname varchar(20) NOT NULL, 
+        lastname varchar(20) NOT NULL,
+        address varchar(100),
+        city varchar(20) NOT NULL,
+        state varchar(20) NOT NULL,
+        zipcode integer(6),
+        phonenumber varchar(10) NOT NULL UNIQUE,
+        email varchar(20) NOT NULL UNIQUE,
+        UNIQUE KEY name (firstname,lastname)
+);
